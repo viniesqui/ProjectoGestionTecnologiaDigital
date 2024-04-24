7ac18058ccaa674a14fc5d560702af936adc98e8
viniesqui <viniesqui@gmail.com>
2024-04-24 15:12:25 -0600

    primera version se deploya en streamlit con esto deberia servir

---
+++ streamlit.py
@@ -0,0 +1,30 @@
+from joblib import load
+import streamlit as st
+import pandas as pd
+from preprocessing import InputPreprocessor
+
+model = load('model.joblib')
+
+
+model_name = st.text_input("Modelo")
+year = st.number_input("Año", min_value=1900, max_value=2022, step=1)
+milage = st.number_input("Millaje", min_value=0, step=1)
+fuel = st.text_input("Tipo Gasolina")
+
+
+if st.button('Predict'):
+    data = {'model': [model_name],
+        'year': [year],
+        'mileage': [milage],
+        'fuel': [fuel]}
+    df = pd.DataFrame(data)
+    
+    
+    preprocessor = InputPreprocessor(df, columns_to_convert=['model', 'fuel'])
+    df = preprocessor.preprocess_data()
+    
+    
+    prediction = model.predict(df)
+    
+   
+    st.write(f'Predicted Price: {prediction[0]}')
