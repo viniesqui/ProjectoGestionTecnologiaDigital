a85e98ac83649415a6e1c5899c94cb8cda0dc5f5
viniesqui <viniesqui@gmail.com>
2024-04-24 08:35:18 -0600

    cambios para que use gridSearch para encontrar el mejor modelo

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVR
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error
-from joblib import dump
+from joblib import dump, load
 from preprocessing import DataPreprocessor
 from sklearn.model_selection import GridSearchCV
 
@@ -20,7 +20,7 @@ class ModelTrainerRegression:
         best_pipeline = None
 
         for name, pipeline in pipelines.items():
-            model = GridSearchCV(pipeline, param_grids[name], cv=5)
+            model = GridSearchCV(pipeline, param_grids[name], cv=5, n_jobs=-1)
             model.fit(self.X_train, self.y_train)
             predictions = model.predict(self.X_test)
             mse = mean_squared_error(self.y_test, predictions)
@@ -36,18 +36,22 @@ class ModelTrainerRegression:
         dump(model, self.model_filepath)
 
     def run(self):
-        # Preprocess the data
         df = self.preprocessor.df
-        processed_data = self.preprocessor.preprocess_data()
 
-        # Define the model pipeline
+        X = df.drop(self.target_column, axis=1)
+        y = df[self.target_column]
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42)
+
+        self.X_train = self.preprocessor.preprocess_data(self.X_train)
+        self.X_test = self.preprocessor.preprocess_data(self.X_test)
+
         pipelines = {
-            'lr': Pipeline(steps=[('preprocessor', processed_data), ('classifier', LinearRegression())]),
-            'rf': Pipeline(steps=[('preprocessor', processed_data), ('classifier', RandomForestRegressor())]),
-            'svr': Pipeline(steps=[('preprocessor', processed_data), ('classifier', SVR())]),
+            'lr': Pipeline(steps=[('classifier', LinearRegression())]),
+            'rf': Pipeline(steps=[('classifier', RandomForestRegressor())]),
+            'svr': Pipeline(steps=[('classifier', SVR())]),
         }
 
-        # Define the hyperparameters to try for each model
         param_grids = {
             'lr': {
                 'classifier__normalize': [True, False],
@@ -62,17 +66,6 @@ class ModelTrainerRegression:
             },
         }
 
-        # Split the data
-        X = df.drop(self.target_column, axis=1)
-        y = df[self.target_column]
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42)
-
-        # Train the models and benchmark
         best_model = self.train_models(pipelines, param_grids)
 
-        # Serialize the best model
         self.serialize_model(best_model)
-        
-        
-    
