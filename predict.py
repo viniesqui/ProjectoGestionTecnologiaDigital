3f61194bef214ae3ba76d376ae91d82ae346468a
viniesqui <viniesqui@gmail.com>
2024-04-25 14:25:21 -0600

    cambios para que us el preprocessing

--- predict.py
+++ predict.py
@@ -1,16 +1,23 @@
 from joblib import load
 import pandas as pd
+from preprocessing import DataPreprocessor
 
 class ModelPredictor:
-    def __init__(self, model):
+    def __init__(self, model, data, columns_to_remove=[], columns_to_convert=[]):
         self.model = model
+        self.preprocessor = DataPreprocessor(
+            data, columns_to_remove=[], columns_to_convert=[])
 
     def load_model(self):
         self.model = load(self.model)
 
-    def predict(self, input_data):
-        if isinstance(input_data, pd.DataFrame):
-            return self.model.predict(input_data)
+    def predict(self):
+        if isinstance(self.data, pd.DataFrame):
+            # Transform the columns to numeric
+            self.preprocessor.df = self.data
+            self.preprocessor.transform_columns_to_numeric(self.data.columns)
+            preprocessed_data = self.preprocessor.df
+
+            return self.model.predict(preprocessed_data)
         else:
             raise TypeError("Input data should be a pandas DataFrame")
-
