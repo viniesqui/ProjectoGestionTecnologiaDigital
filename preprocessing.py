db1b3fe4d2a28766df7f0cfda31efa8633dfcb42
viniesqui <viniesqui@gmail.com>
2024-04-25 13:29:25 -0600

    cambios para tener los encoders y que se pueda darle un df y no el archivo csv

--- preprocessing.py
+++ preprocessing.py
@@ -1,18 +1,18 @@
 import pandas as pd
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OrdinalEncoder
-from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder
 
-class CustomLabelEncoder(LabelEncoder):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
 
-    def inverse_transform(self, X):
-        return super().inverse_transform(X)
+class ReadCsv:
+    def __init__(self, data_filepath):
+        self.data_filepath = data_filepath
+
+    def read_data(self):
+        return pd.read_csv(self.data_filepath)
 
 class DataPreprocessor:
-    def __init__(self, filepath, columns_to_remove=[], columns_to_convert=[]):
-        self.df = pd.read_csv(filepath)
+    def __init__(self, data, columns_to_remove=[], columns_to_convert=[]):
+        self.df = data #quitamos la dependencia de un archivo csv, ahora se da el dataframe
+        self.encoders = {}  # se le mete esto para hacerle el inverso
         self.remove_columns(columns_to_remove)
         self.transform_columns_to_numeric(columns_to_convert)
 
@@ -20,69 +20,13 @@ class DataPreprocessor:
         self.df = self.df.drop(columns, axis=1)
 
     def transform_columns_to_numeric(self, columns):
-        le = CustomLabelEncoder()
         for column in columns:
+            le = LabelEncoder()
             self.df[column] = le.fit_transform(self.df[column])
+            self.encoders[column] = le 
 
-    def preprocess_data(self):
-        numeric_features = self.df.select_dtypes(
-            include=['int64', 'float64']).columns
-        categorical_features = self.df.select_dtypes(
-            include=['object']).columns
+    def get_data(self):
+        return self.df
 
-        numeric_transformer = Pipeline(steps=[
-            ('scaler', StandardScaler())
-        ])
-
-        categorical_transformer = Pipeline(steps=[
-            ('encoder', OrdinalEncoder())
-        ])
-
-        preprocessor = ColumnTransformer(
-            transformers=[
-                ('num', numeric_transformer, numeric_features),
-                ('cat', categorical_transformer, categorical_features)
-            ])
-
-        preprocessed_data = preprocessor.fit_transform(self.df)
-
-        return preprocessed_data
-        
-        
-class InputPreprocessor:
-    def __init__(self, data):
-        self.df = data
-
-    def transform_columns_to_numeric(self, columns):
-        for column in columns:
-            self._encode_column(column)
-
-    def _encode_column(self, column):
-        le = LabelEncoder()
-        self.df[column] = le.fit_transform(self.df[column])
-
-    def preprocess_data(self):
-        numeric_features = self._get_numeric_features()
-        preprocessed_data = self._apply_preprocessing(numeric_features)
-        return preprocessed_data
-
-    def _get_numeric_features(self):
-        numeric_features = self.df.columns
-        numeric_features = [feature for feature in numeric_features if self.df[feature].nunique() > 1]
-        return numeric_features
-
-    def _apply_preprocessing(self, numeric_features):
-        if not numeric_features:
-            return self.df
-
-        numeric_transformer = Pipeline(steps=[
-            ('scaler', StandardScaler())
-        ])
-
-        preprocessor = ColumnTransformer(
-            transformers=[
-                ('num', numeric_transformer, numeric_features)
-            ])
-
-        preprocessed_data = preprocessor.fit_transform(self.df)
-        return preprocessed_data
+    def get_encoders(self):
+        return self.encoders
