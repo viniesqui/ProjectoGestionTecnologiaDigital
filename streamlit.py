dc318a614778a2f328b8cf60368c01616aa24708
viniesqui <viniesqui@gmail.com>
2024-04-25 14:18:41 -0600

    cambios para reflejar nuvo vodigo

--- streamlit.py
+++ streamlit.py
@@ -1,37 +1,31 @@
-import joblib
 import streamlit as st
+import joblib
 import pandas as pd
-from preprocessing import InputPreprocessor
+from predict import ModelPredictor
 
+# cargar el modelo
 model = joblib.load('model.joblib')
 
+
+#hacerle los inputs
 model_name = st.text_input("Modelo")
 year = st.number_input("Año", min_value=1900, max_value=2022, step=1)
 milage = st.number_input("Millaje", min_value=0, step=1)
 fuel = st.text_input("Tipo Gasolina")
 
+# crear dataframe
+input_data = pd.DataFrame({
+    'model': [model_name], 
+    'year': [year], 
+    'mileage': [milage], 
+    'fuel': [fuel]
+})
 
-def predict_price(model, model_name, year, milage, fuel):
-    data = {'model': [model_name],
-            'year': [year],
-            'mileage': [milage],
-            'fuel': [fuel]}
-    df = pd.DataFrame(data)
-    print(df, "asi entra")
-    preprocessor = InputPreprocessor(df)
-    preprocessor.transform_columns_to_numeric(['model', 'fuel'])
-    preprocessed_data = preprocessor.preprocess_data()
-    print(preprocessed_data, "asi sale")
+# Create a new ModelPredictor instance
+predictor = ModelPredictor(model)
 
-    # Check number of features
-    if preprocessed_data.shape[1] != model.n_features_in_:
-        raise ValueError(
-            f"Input data has {preprocessed_data.shape[1]} features, but model expects {model.n_features_in_} features")
+# Use the predictor to make a prediction
+prediccion = predictor.predict(input_data)
 
-    # Make prediction
-    prediction = model.predict(preprocessed_data)
-    return prediction[0]
 
-if st.button('Predict'):
-    predicted_price = predict_price(model, model_name, year, milage, fuel)
-    st.write(f'Predicted Price: {predicted_price}')
+st.write(f"El precio estimado es: {prediccion[0]}")
